b4da578b55ef4f16d04999063c31e0a7025ddd47
elcerdo <georges.wbushjr@laposte.net>
2009-06-03 20:32:35 +0200

    works!!

--- client.py
+++ client.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
 
+"""
+A simple echo client that handles some exceptions
+"""
+
 import socket
+import sys
 
-ss=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-ss.bind(("localhost",9998))
-ss.send("coucou")
-data=ss.recv(1024)
+host = 'localhost'
+port = 9999
+size = 1024
+s = None
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host,port))
+except socket.error, (value,message):
+    if s:
+        s.close()
+    print "Could not open socket: " + message
+    sys.exit(1)
+s.send('Hello, world')
+data = s.recv(size)
 s.close()
-
-print "got back %s" % data
-
+print 'Received:', data
--- server.py
+++ server.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python
 
-import socket
+"""
+A simple echo server that handles some exceptions
+"""
 
-ss=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-ss.bind(("localhost",9999))
-ss.listen(3)
-while True:
-    client,adrr=ss.accept()
-    print client,adrr
-    data=client.recv(1024)
-    print data
+import socket
+import sys
+
+host = ''
+port = 9999
+backlog = 5
+size = 1024
+s = None
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+    s.bind((host,port))
+    s.listen(backlog)
+except socket.error, (code,message):
+    if s:
+        s.close()
+    print "Could not open socket: " + message
+    sys.exit(1)
+while 1:
+    client, address = s.accept()
+    data = client.recv(size)
+    print "Received: " + data
     if data:
         client.send(data)
     client.close()
-    
-
-
