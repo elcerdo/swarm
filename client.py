f73bb7d9c95b9c38a81c5a199b9576883ee815a9
pierre <pierre@sd-12155.dedibox.fr>
2009-06-13 17:37:58 +0200

    updated sort in client

--- client.py
+++ client.py
@@ -140,7 +140,7 @@ class DataPing:
             if self.last_ping is None and y.last_ping is not None:
                 return 1
             if self.last_ping is not None and y.last_ping is not None:
-                return -cmp(self.last_ping,y.last_ping)
+                return -cmp(self.count,y.count)
 
         def __repr__(self):
             base="name=%s ip=%s" % (self.name,self.ip)
@@ -176,6 +176,7 @@ class DataPing:
         if self.pinger is None and self.links:
             candidates = self.links.values()
             candidates.sort()
+            #print "Launching pinger on link %s" % candidates[-1]
             self.pinger = DataPing.Pinger(candidates[-1])
             self.pinger.start()
         
