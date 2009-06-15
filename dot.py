2b48b396d32481c86afa96dc1a74c64934bf48fd
elcerdo <georges.wbushjr@laposte.net>
2009-06-15 13:47:56 +0200

    updated dot script

--- dot.py
+++ dot.py
@@ -18,24 +18,43 @@ if __name__=="__main__":
     file.close()
 
     dot=pydot.Dot()
-    dot.set_type('digraph')
+    dot.set_prog('neato')
+    dot.set_type('graph')
 
+    times={}
     nodes={}
     for (sid,rid),(sn,rn,ss,time) in data.items():
-        if sid not in nodes:
+        if sid==rid: #parse self pinging node
+            assert(ss)
+            assert(sn==rn)
             node=pydot.Node(sn.split('.')[0])
+            nodes[sid]=(time,node)
             dot.add_node(node)
-            nodes[sid]=node
-        if rid not in nodes:
-            node=pydot.Node(rn.split('.')[0])
-            dot.add_node(node)
-            nodes[rid]=node
-        
-        if ss:
-                edge=pydot.Edge(nodes[sid],nodes[rid])
-                edge.set_label("%.1f" % time)
-                edge.set_len("%f" % time)
-                dot.add_edge(edge)
+            del data[(sid,rid)]
+        elif not ss: #remove failed ping
+            try:
+                del data[(sid,rid)]
+                del data[(rid,sid)]
+            except KeyError:
+                pass
+
+    edges={}
+    while data:
+        (sid,rid),(sn,rn,ss,time)=data.popitem()
+        assert(ss)
+        edges[frozenset((sid,rid))]=(nodes[sid][1],nodes[rid][1],(time-nodes[sid][0]+data[(rid,sid)][3]-nodes[rid][0])/2)
+        del data[(rid,sid)]
+
+    max_time=max(time for sn,rn,time in edges.values())
+    min_time=min(time for sn,rn,time in edges.values())
+    def clamp(time,min_len=2,max_len=5):
+        return min_len+(max_len-min_len)*(time-min_time)/(max_time-min_time)
+
+    for sn,rn,time in edges.values():
+        edge=pydot.Edge(sn,rn)
+        edge.set_label("%.2f" % time)
+        edge.set_len("%f" % clamp(time))
+        dot.add_edge(edge)
 
     dot.write(options.output,format="png")
 
